f3eefd8e1d2519749d0fe7dac74895895801c6fa
firyalmahakhan <firyalmahak@gmail.com>
2018-08-09 21:13:57 +0000

    Done.

--- weather.rb
+++ weather.rb
@@ -3,39 +3,45 @@
 weather = {
   :city => "New York",
   :description => "Partly Cloudy",
-  :temperature_farenheit => 66.3
+  :temperature_farenheit => "66.3"
   }
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
 
-def location(@nyc_weather)
-	puts "New York"["66.3"]
+def location(weather)
+	puts weather_description(temperature_farenheit)
 end
 
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
-def description
-  puts description["Partly Cloudy"]
+def weather_description(weather)
+  return weather[description]
 end
-
+weather_description
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 
 def weather_report 
-  puts "The weather in New York is #{description}. The temperature is #{weather} degrees Farenheit." 
+  return "The weather in New York is #{description}. The temperature is #{weather} degrees Farenheit." 
 end 
 
 #4. Write a method that takes in the weather hash as an argument. 
 # You'll want to use the each method to iterate over the hash and print out
-# `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
+[The city is New York, The description is Partly Cloudy, The temperature is 66.3.]
+
 weather_hash 
   puts "The city is #{city}. The description is #{description}. The temperature is #{temperature_farenheit}."
+end 
 
 
 # BONUS:  Write a method that takes in the weather hash as an argument and converts the farenheit temperature to celcius. Have the method round your answer to two decimal places. Is there a method we can use to round in Ruby?
 
 # Hint - how can we find the algorithm to convert Farenheit to Celsius?
 
+def convert 
+  puts "#{Celsius} => #{Farenheit} * (9/5 + 32)
+  end 
+end 
 
 
